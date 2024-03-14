d5ab7921c1606205c75eeba821303456d7e2ca7b
Pedro Henrique <133764832+pedrobezerra14@users.noreply.github.com>
2024-03-14 07:00:10 -0300

    Create area.py

---
+++ conceitos-basicos/area.py
@@ -0,0 +1,5 @@
+base = float(input('Digite a base do seu triângulo:'))
+altura = float(input('Digite a altura do seu triângulo:'))
+
+area = base * altura/2
+print('A base do seu triângulo é:',area,'centímetros quadrados')
