864e5d584aba6905d2fbc3b2f8423f423fc5516f
Pedro Henrique <133764832+pedrobezerra14@users.noreply.github.com>
2024-03-13 22:12:04 -0300

    algoritmo media

---
+++ algoritmo/media.py
@@ -0,0 +1,7 @@
+n1 = float(input("Adicione a primeira nota do aluno: "))
+n2 = float(input("Adicione a segunda nota do aluno: "))
+n3 = float(input("Adicione a terceira nota do aluno: "))
+
+media = ( n1 + n2 + n3 ) / 3
+
+print("A média do aluno de acordo com as notas, será: ",media)
