bddb25e5c5dd202d4ff66d16f1155c6a3b799b88
Pedro Henrique <133764832+pedrobezerra14@users.noreply.github.com>
2024-03-14 11:12:38 -0300

    Update desconto.py

--- conceitos-basicos/desconto.py
+++ conceitos-basicos/desconto.py
@@ -3,3 +3,9 @@
 # percentual de desconto. Exiba o valor
 # do desconto e o preço a pagar.
 
+preco_mercadoria = float(input('Informe o preço do produto que deseja aplicar o desconto: '))
+percentual_desconto = float(input('Informe o percentual de desconto desejado: '))
+
+desconto = preco_mercadoria * ( percentual_desconto / 100 )
+
+print(f'O valor do desconto foi de {percentual_desconto}% e o preço a pagar será de R${desconto}')
