d0e673866645fe7d98f31d034c724f0300da1ac4
Pedro Henrique <133764832+pedrobezerra14@users.noreply.github.com>
2024-03-14 10:55:21 -0300

    Update imc3.py

--- conceitos-basicos/imc3.py
+++ conceitos-basicos/imc3.py
@@ -5,10 +5,24 @@ imc = peso / altura **2
 
 imc_limitado = round(imc, 2)
 
-
+print("---------------------------------")
+print(f"Seu IMC é {imc_limitado}")
+print("---------------------------------")
 
 if imc_limitado < 18.5:
     print(f"Por seu IMC estar abaixo de 18.5, você se enquadra em MAGREZA")
+elif imc_limitado >= 18.5 and imc_limitado <= 24.9:
+    print(f"Por seu IMC estar entre de 18.5 e 24.9, você se enquadra em NORMAL")
+elif imc_limitado >= 25.0 and imc_limitado <= 29.9:
+    print(f"Por seu IMC estar entre de 25.0 e 29.9, você se enquadra em SOBREPESO")
+elif imc_limitado >= 30.0 and imc_limitado <= 39.9:
+    print(f"Por seu IMC estar entre de 18.5 e 24.9, você se enquadra em OBESIDADE")
+elif imc_limitado >= 40.0:
+    print(f"Por seu IMC ser maior que 40, você se enquadra em OBESIDADE GRAVE")
+
+print("---------------------------------")
+print("INFORMAÇÕES DE ACORDO COM A TABELA ABAIXO")
+print("---------------------------------")
 
 def tabelaImc():
     print("IMC   \t\tClassificação")
