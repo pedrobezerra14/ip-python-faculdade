62c54f9ee2032a496609eadf62cbe50421ade17a
Pedro Henrique <133764832+pedrobezerra14@users.noreply.github.com>
2024-03-14 11:24:19 -0300

    Update viagem.py

--- conceitos-basicos/viagem.py
+++ conceitos-basicos/viagem.py
@@ -1,7 +1,12 @@
+# 4. Escreva um programa que calcule o
+# tempo de uma viagem de carro.
+# Pergunte a distância a percorrer e a
+# velocidade média esperada para a
+# viagem.
 
+distancia = float(input('Informe a distância que irá percorrer: '))
+velocidade_media = float(input('Informe a velocidade média para a viagem: '))
 
-4. Escreva um programa que calcule o
-tempo de uma viagem de carro.
-Pergunte a distância a percorrer e a
-velocidade média esperada para a
-viagem.
+tempo = distancia / velocidade_media
+
+print(f'O tempo de sua viagem será de {tempo}hrs')
