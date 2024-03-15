2ada661241fe9f1f93c91da64b046560f023e65c
Pedro Henrique <133764832+pedrobezerra14@users.noreply.github.com>
2024-03-15 06:37:15 -0300

    create ex1.py

---
+++ estudo-dirigido/ex1.py
@@ -0,0 +1,7 @@
+# 1. Faça um programa que peça dois números inteiros. Imprima a soma
+# desses dois números na tela.
+
+num1 = int(input('Informe um número inteiro: '))
+num2 = int(input('Informe outro número inteiro: '))
+
+print(f'Os números inteiros informados foram: {num1} e {num2}')
