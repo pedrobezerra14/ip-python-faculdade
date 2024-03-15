e632c2b45c02b9fb4b937f9464a4a8cd74e2a172
Pedro Henrique <133764832+pedrobezerra14@users.noreply.github.com>
2024-03-15 07:12:21 -0300

    Update ex7.py

--- estudo-dirigido/ex7.py
+++ estudo-dirigido/ex7.py
@@ -1,3 +1,8 @@
 # 7. Faça um programa que calcule o volume de um cilindro, dado pela
 # fórmula V = π. r2. h. Solicite ao usuário o raio da base e a altura do
 # cilindro.
+
+raio_da_base = float(input('Informe o raio da base do cilindro: '))
+altura = float(input('Informe a altura do cilindro: '))
+
+volume = 3.14 * raio_da_base **2 
