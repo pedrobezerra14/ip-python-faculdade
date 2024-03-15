902fdba7fadf90be724fe7becc6fad601c8d5a3d
Pedro Henrique <133764832+pedrobezerra14@users.noreply.github.com>
2024-03-15 06:45:48 -0300

    create ex3.py

---
+++ estudo-dirigido/ex3.py
@@ -0,0 +1,6 @@
+# 3. Escreva um programa que calcule um aumento de 15% para um salário
+# de R$ 750.
+
+aumento = ( 750 * ( 15 / 100) ) + 750
+
+print(f'Seu salário após o aumento será de R${aumento}')
