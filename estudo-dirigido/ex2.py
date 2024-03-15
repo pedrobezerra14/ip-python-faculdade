0d03280a260f884ef093cbf451373af2f1793718
Pedro Henrique <133764832+pedrobezerra14@users.noreply.github.com>
2024-03-15 06:40:40 -0300

    create ex2.py

---
+++ estudo-dirigido/ex2.py
@@ -0,0 +1,7 @@
+# 2. Escreva um programa que leia um valor em metros e o exiba convertido
+# em milímetros.
+
+metros = float(input('Informe o valor desejado em metros: '))
+milimetros = metros * 1000
+
+print(f'O valor de {metros} metros convertido para milimetros é {milimetros}mm')
