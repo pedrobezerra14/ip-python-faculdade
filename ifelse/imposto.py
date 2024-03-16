ad12a034ff45c354244933a8315464d8d06c341e
Pedro Henrique <133764832+pedrobezerra14@users.noreply.github.com>
2024-03-16 07:19:57 -0300

    Update imposto.py

---
+++ ifelse/imposto.py
@@ -0,0 +1,6 @@
+salario = float(input('Informe seu salário: '))
+
+if salario >= 1200:
+    print(f'Seu salário é de {salario} então você deverá pagar imposto!')
+else:
+    print(f'Seu salário é menor que 1200 reais, então você não deve pagar impostos!')
