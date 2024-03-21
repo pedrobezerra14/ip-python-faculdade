9d90840b63d89f1f26da638d25eac39eef75feab
Pedro Henrique <133764832+pedrobezerra14@users.noreply.github.com>
2024-03-21 19:17:32 -0300

    Update ex9.py

--- conceitos-basicos/estudo-dirigido/ex9.py
+++ conceitos-basicos/estudo-dirigido/ex9.py
@@ -1,2 +1,6 @@
 # 9. Escreva um programa que leia a quantidade de dias, horas, minutos e
 # segundos do usuário. Calcule o total em segundos.
+
+dias = int(input('Insira a quantidade de dias desejado: '))
+horas = int(input('Insira a quantidade de horas desejado: '))
+minutos = int(input('Insira a quantidade de minutos desejado: '))
