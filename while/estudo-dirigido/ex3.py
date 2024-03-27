6bd9c91347f735bae373b9631933da8d6fa98a59
Pedro Henrique <133764832+pedrobezerra14@users.noreply.github.com>
2024-03-27 10:42:11 -0300

    Update ex3.py

--- while/estudo-dirigido/ex3.py
+++ while/estudo-dirigido/ex3.py
@@ -1,9 +1,7 @@
 senha = input('Informe uma senha de pelo menos 8 caracteres: ')
-tamanho_senha = len(senha)
 
-while tamanho_senha < 8:
-    print('Digite uma senha com pelo menos 8 caracteres!!!')
+while len(senha) < 8:
+    senha = input('Informe uma senha de pelo menos 8 caracteres: ')
     break
-while tamanho_senha >= 8:
-    print('Senha inserida com sucesso!')
-    break
+
+print('Senha cadastrada com sucesso!')  
