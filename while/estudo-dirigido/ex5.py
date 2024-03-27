a90d6bc9ce1a2cb7ab7bf531fb27df2d3d2ea53f
Pedro Henrique <133764832+pedrobezerra14@users.noreply.github.com>
2024-03-27 10:59:08 -0300

    modifing ex5.py

--- while/estudo-dirigido/ex5.py
+++ while/estudo-dirigido/ex5.py
@@ -4,11 +4,11 @@
 
 import random
 
-num = int(input('Informe um número inteiro aleatório entre 1 e 100:'))
+num = int(input('Informe um número inteiro aleatório entre 1 e 100: '))
 
 numero_aleatorio = random.randint(1, 100)
 
 while num != numero_aleatorio:
-    num = int(input('Informe um número inteiro aleatório entre 1 e 100:'))
+    num = int(input('Informe um número inteiro aleatório entre 1 e 100: '))
     if num == numero_aleatorio:
         print('Você acertou!')
--- while/estudo-dirigido/ex6.py
+++ while/estudo-dirigido/ex6.py
@@ -1,2 +1,4 @@
 # Escreva um algoritmo que solicite ao usuário o primeiro termo, a razão e o número de
 # termos de uma progressão aritmética. Imprima os termos da progressão.
+
+primeiro_termo = float(input('Informe o primeiro termo da PA: '))
