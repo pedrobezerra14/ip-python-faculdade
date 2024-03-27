670ba98c7fffc1beed8bc8336aa3e3426e975887
Pedro Henrique <133764832+pedrobezerra14@users.noreply.github.com>
2024-03-27 11:05:51 -0300

    Update ex6.py

--- while/estudo-dirigido/ex6.py
+++ while/estudo-dirigido/ex6.py
@@ -2,3 +2,15 @@
 # termos de uma progressão aritmética. Imprima os termos da progressão.
 
 primeiro_termo = float(input('Informe o primeiro termo da PA: '))
+razao = float(input('Informe a razão da PA: '))
+num_termos = float(input('Informe o número de termos da PA: '))
+termo_atual = primeiro_termo
+contador = 0
+
+print("Os termos da progressão aritmética são:")
+
+while contador < num_termos:
+    print(termo_atual)
+    termo_atual += razao
+    contador += 1
+    
