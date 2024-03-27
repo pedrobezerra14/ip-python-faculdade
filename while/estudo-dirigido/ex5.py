907d32792f2baf77246115c5d31a8e6ecc7a0411
Pedro Henrique <133764832+pedrobezerra14@users.noreply.github.com>
2024-03-27 10:56:57 -0300

    Update ex5.py

--- while/estudo-dirigido/ex5.py
+++ while/estudo-dirigido/ex5.py
@@ -1,3 +1,14 @@
 # Crie um jogo em que o computador escolhe um número aleatório entre 1 e 100, e o jogador
 # tem que adivinhar o número. Permita que o jogador continue adivinhando até acertar o
 # número.
+
+import random
+
+num = int(input('Informe um número inteiro aleatório entre 1 e 100:'))
+
+numero_aleatorio = random.randint(1, 100)
+
+while num != numero_aleatorio:
+    num = int(input('Informe um número inteiro aleatório entre 1 e 100:'))
+    if num == numero_aleatorio:
+        print('Você acertou!')
