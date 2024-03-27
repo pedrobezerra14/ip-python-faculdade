f3b2d651b1cb12674d51aa3f587469dff57b518e
Pedro Henrique <133764832+pedrobezerra14@users.noreply.github.com>
2024-03-27 10:45:53 -0300

    Create ex5.py

---
+++ while/estudo-dirigido/ex5.py
@@ -0,0 +1,3 @@
+# Crie um jogo em que o computador escolhe um número aleatório entre 1 e 100, e o jogador
+# tem que adivinhar o número. Permita que o jogador continue adivinhando até acertar o
+# número.
