6021c22d2ab29d846d9cbedc0a4ee51747ced4d1
Pedro Henrique <133764832+pedrobezerra14@users.noreply.github.com>
2024-04-10 23:46:16 -0300

    Update ex5.py

--- while/estudo-dirigido/ex5.py
+++ while/estudo-dirigido/ex5.py
@@ -5,10 +5,18 @@
 import random
 
 num = int(input('Informe um número inteiro aleatório entre 1 e 100: '))
-
 numero_aleatorio = random.randint(1, 100)
+contador = 1
+print(numero_aleatorio)
 
 while num != numero_aleatorio:
-    num = int(input('Informe um número inteiro aleatório entre 1 e 100: '))
-    if num == numero_aleatorio:
-        print('Você acertou!')
+    if num < 1 or num > 100:
+        print('O número informado está dentro do intervalo de 1 e 100.')
+        num = int(input('Informe um número inteiro aleatório entre 1 e 100: '))
+    else:
+        print('Você errou, tente novamente!')
+        num = int(input('Informe um número inteiro aleatório entre 1 e 100: '))
+contador += 1
+
+print(f'Você acertou! O número sorteado foi {numero_aleatorio}')
+print(f'Você tentou {contador} vezes')
