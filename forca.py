c7f0ad10f210c03bfe108e441346a7600c4569dd
Pedro Henrique <133764832+pedrobezerra14@users.noreply.github.com>
2024-04-22 21:36:10 -0300

    adding ex7.py

--- forca.py
+++ forca.py
@@ -1,8 +1,6 @@
-# O Jogo da Forca é um jogo clássico em que um jogador tenta
-# adivinhar uma palavra oculta, letra por letra, antes que seu boneco
-# seja desenhado na forca. O programa deve gerar uma palavra
-# aleatória a ser adivinhada pelo jogador e permitir que o jogador insira
-# letras para tentar adivinhar a palavra.
+# O programa deve:
+# 1- gerar uma palavra aleatória a ser adivinhada pelo jogador
+# 2- permitir que o jogador insira letras para tentar adivinhar a palavra.
 
 import random
 
@@ -12,6 +10,51 @@ palavra_selecionada = random.choice(palavras_para_adivinhar)
 letras_adivinhadas = []
 tentativas_totais = 6
 
-resultado = ''
-for letra in palavra_selecionada:
-    resultado += '_ '
+boneco_forca = [
+    "  ____\n |    |\n      |\n      |\n      |\n      |\n______|",
+    "  ____\n |    |\n O    |\n      |\n      |\n      |\n______|",
+    "  ____\n |    |\n O    |\n |    |\n      |\n      |\n______|",
+    "  ____\n |    |\n O    |\n/|    |\n      |\n      |\n______|",
+    "  ____\n |    |\n O    |\n/|\   |\n      |\n      |\n______|",
+    "  ____\n |    |\n O    |\n/|\   |\n/     |\n      |\n______|",
+    "  ____\n |    |\n O    |\n/|\   |\n/ \   |\n      |\n______|"
+]
+
+# Inicializa a string resultado com underscores para cada letra da palavra selecionada
+resultado = '_' * len(palavra_selecionada)
+
+print('VOCÊ TEM 6 TENTATIVAS PARA DESCOBRIR A PALAVRA!')
+
+while True:
+    print(resultado)
+    
+    letra = input("Digite uma letra para saber se ela está na palavra: ").lower()
+    if len(letra) != 1:
+        print("Por favor, digite apenas um caractere por vez.")
+        continue
+
+    if letra in letras_adivinhadas:
+        print("Você já tentou essa letra!")
+        continue
+
+    letras_adivinhadas.append(letra)
+
+    if letra not in palavra_selecionada:
+        tentativas_totais -= 1
+        print(f"ERROU! Agora você tem {tentativas_totais} tentativas restantes.")
+        print(boneco_forca[6 - tentativas_totais])
+
+        if tentativas_totais == 0:
+            print(f"Infelizmente você perdeu! A palavra era: {palavra_selecionada}")
+            break
+    else:
+        resultado = ''
+        for letra_palavra in palavra_selecionada:
+            if letra_palavra in letras_adivinhadas:
+                resultado += letra_palavra
+            else:
+                resultado += '_'
+                
+        if '_' not in resultado:
+            print(f"Parabéns! Você adivinhou a palavra: {palavra_selecionada}!")
+            break
--- while/estudo-dirigido/ex7.py
+++ while/estudo-dirigido/ex7.py
@@ -3,15 +3,20 @@
 
 saldo = 0
 
-operacao = input('Informe a operação que deseja realizar: ')
-
-if operacao == 'saldo':
-    print(f'Você tem R${saldo}')
-elif operacao == 'deposito':
-    valor_deposito = float(input('Informe o valor que deseja depositar: '))
-    deposito = saldo + valor_deposito
-    print(f'Seu saldo atual é de R${deposito}')
-elif operacao == 'saque':
-    valor_saque = float(input('Informe o valor que deseja sacar: '))
-    saque = saldo - valor_saque
-    print(f'Seu saldo atual é de R${saque}')
+while True:
+    operacao = input('Informe a operação que deseja realizar: ')    
+    if operacao == 'consulta':
+        print(f'Você tem R${saldo}')
+    elif operacao == 'deposito':
+        valor_deposito = float(input('Informe o valor que deseja depositar: '))
+        deposito = saldo + valor_deposito
+        saldo += deposito
+        print(f'Seu saldo atual é de R${deposito}')
+    elif operacao == 'saque':
+        valor_saque = float(input('Informe o valor que deseja sacar: '))
+        saque = saldo - valor_saque
+        saldo -= saque
+        print(f'Seu saldo atual é de R${saque}')
+    else:
+        print('Fechando operação')
+        break
